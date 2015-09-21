bc807ba6770ef94dd603b89a8c6eae61cf9b3033
kaishuoy <kaishuoy@yahoo.com>
2015-09-21 18:31:51 +1200

    corrected implementation for stairs

--- EclipseWorkspace/ArtGame/src/artGame/game/StairTile.java
+++ EclipseWorkspace/ArtGame/src/artGame/game/StairTile.java
@@ -29,5 +29,19 @@ public class StairTile extends Tile{
 		this.linkedTile = linkedTile;
 	}
 	
+	/**
+	 * when we try to normally move a character on to this tile,
+	 * we should instead move it to the linked tile
+	 */
+	@Override
+	public void setOccupant(Character occupant){
+		this.linkedTile.setOccupantHelper(occupant);
+	}
+	/**
+	 * actual method to set the occupant
+	 */
+	public void setOccupantHelper(Character occupant){
+		this.occupant = occupant;
+	}
 
 }
