ded5405bbd6db1410ae9578abf0ba9436070d493
Tim King <king.timothycw@gmail.com>
2015-10-04 13:07:06 +1300

    Added GameData shell for client to use

---
+++ EclipseWorkspace/ArtGame/src/artGame/ui/GameData.java
@@ -0,0 +1,52 @@
+package artGame.ui;
+
+import artGame.game.Guard;
+import artGame.game.Item;
+import artGame.game.Player;
+import artGame.game.Sculpture;
+import artGame.game.Tile;
+import artGame.game.Wall;
+
+/**
+ * Used by the client to get information about the current gamestate over the network.
+ * @author Tim
+ *
+ */
+public class GameData {
+
+	public static Player getCurrentPlayer(){
+		
+		return null;
+	}
+	
+	public static Player[] getPlayers(){
+		
+		return null;
+	}
+	
+	public static Guard[] getGuards(){
+		
+		return null;
+	}
+	
+	public static Tile[] getTiles(){
+	
+		return null;
+	}
+	
+	public static Item[] getCurrentItems(){
+		
+		return null;
+	}
+	
+	public Wall[] getWallData(){
+		
+		return null;
+	}
+	
+	public Sculpture[] getSculptures(){
+		
+		return null;
+	}
+	
+}
